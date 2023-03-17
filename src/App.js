2f0daa4a4173f4638ef16a7705e4a7bd4115f9fa
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2023-03-16 21:24:44 -0600

    exercise 2.6-2.10

--- src/App.js
+++ src/App.js
@@ -1,53 +1,46 @@
-import Course from './components/Course'
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
     }
-  ]
+
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${personObject.name} is already added to phonebook`)
+      setNewName('')
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
-    courses.map(course => <Course key={course.id} course={course} />)
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange}/>
+        </div>
+        <div>number: <input value={newName} onChange={handlePersonChange} /></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
   )
 }
 
